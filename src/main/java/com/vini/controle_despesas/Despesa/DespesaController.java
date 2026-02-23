976a67e20ab38731a2020da77e25b670d094845e
Vincius <viniviniee11@gmail.com>
2026-02-23 12:11:28 -0300

    feat: cria a camada Controller para controlar o fluxo da api

---
+++ src/main/java/com/vini/controle_despesas/Despesa/DespesaController.java
@@ -0,0 +1,13 @@
+package com.vini.controle_despesas.Despesa;
+
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/despesa")
+public class DespesaController {
+
+
+}
