8157a1304872e59d5ee1e37ed8aa954d1c67260c
Vincius <viniviniee11@gmail.com>
2026-02-23 12:10:32 -0300

    fix: associa o tipo DespesaModel ao DespesaRepository

---
+++ src/main/java/com/vini/controle_despesas/Despesa/DespesaRepository.java
@@ -0,0 +1,6 @@
+package com.vini.controle_despesas.Despesa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DespesaRepository extends JpaRepository<DespesaModel, Long> {
+}
