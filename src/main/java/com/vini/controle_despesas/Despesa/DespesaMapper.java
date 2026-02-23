930b0c3b0a57c5615b860d8ae05c2a13e10c9d7a
Vincius <viniviniee11@gmail.com>
2026-02-23 10:42:49 -0300

    feat: adiciona mapper para conversão entre model e dto

---
+++ src/main/java/com/vini/controle_despesas/Despesa/DespesaMapper.java
@@ -0,0 +1,41 @@
+package com.vini.controle_despesas.Despesa;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DespesaMapper {
+
+    public DespesaModel map(DespesaDTO despesaDTO){
+
+        DespesaModel despesaModel = new DespesaModel();
+
+        despesaModel.setId(despesaDTO.getId());
+        despesaModel.setId(despesaDTO.getId());
+        despesaModel.setDescricao(despesaDTO.getDescricao());
+        despesaModel.setValor(despesaDTO.getValor());
+        despesaModel.setTipo(despesaDTO.getTipo());
+        despesaModel.setPrioridade(despesaDTO.getPrioridade());
+        despesaModel.setStatus(despesaDTO.getStatus());
+        despesaModel.setDataVencimento(despesaDTO.getDataVencimento());
+
+        return despesaModel;
+    }
+
+    public DespesaDTO map(DespesaModel despesaModel){
+
+        DespesaDTO despesaDTO = new DespesaDTO();
+
+        despesaDTO.setId(despesaModel.getId());
+        despesaDTO.setDescricao(despesaModel.getDescricao());
+        despesaDTO.setValor(despesaModel.getValor());
+        despesaDTO.setTipo(despesaModel.getTipo());
+        despesaDTO.setPrioridade(despesaModel.getPrioridade());
+        despesaDTO.setStatus(despesaModel.getStatus());
+        despesaDTO.setDataVencimento(despesaModel.getDataVencimento());
+
+        return despesaDTO;
+    }
+
+
+
+}
