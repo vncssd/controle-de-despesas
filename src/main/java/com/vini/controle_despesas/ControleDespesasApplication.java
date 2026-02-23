c29facceade89dedc6bbf93bbcf3d09064fd51e6
Vincius <viniviniee11@gmail.com>
2026-02-23 09:21:08 -0300

    initial commit

---
+++ src/main/java/com/vini/controle_despesas/ControleDespesasApplication.java
@@ -0,0 +1,13 @@
+package com.vini.controle_despesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDespesasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vini/controle_despesas/ControleDespesasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vini.controle_despesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDespesasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
