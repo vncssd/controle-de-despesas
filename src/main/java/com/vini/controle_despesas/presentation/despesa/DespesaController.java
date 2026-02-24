ef02d5e1021623a5f0200e84d3da0172ec0111f0
Vincius <viniviniee11@gmail.com>
2026-02-24 16:19:29 -0300

    feat: cria os endpoints para criar e listar despesas

--- src/main/java/com/vini/controle_despesas/presentation/despesa/DespesaController.java
+++ src/main/java/com/vini/controle_despesas/presentation/despesa/DespesaController.java
@@ -1,11 +1,39 @@
-package com.vini.controle_despesas.presentation.controller;
+package com.vini.controle_despesas.presentation.despesa;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.vini.controle_despesas.application.despesa.DespesaDTO;
+import com.vini.controle_despesas.domain.despesa.usecase.adicionar.AdicionarDespesaUseCase;
+import com.vini.controle_despesas.domain.despesa.usecase.listar.ListarDespesasUseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/despesa")
 public class DespesaController {
+
+    private AdicionarDespesaUseCase adicionarDespesaUseCase;
+    private ListarDespesasUseCase listarDespesasUseCase;
+
+    public DespesaController(AdicionarDespesaUseCase adicionarDespesaUseCase, ListarDespesasUseCase listarDespesasUseCase) {
+        this.adicionarDespesaUseCase = adicionarDespesaUseCase;
+        this.listarDespesasUseCase = listarDespesasUseCase;
+    }
+
+    @PostMapping("/adicionar")
+    public ResponseEntity<String> criarDespesa (@RequestBody DespesaDTO despesaDTO){
+        despesaDTO = adicionarDespesaUseCase.execute(despesaDTO);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("despesa adicionada " + despesaDTO.getDescricao());
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List> listarDespesas (){
+        List<DespesaDTO> despesasList = listarDespesasUseCase.execute();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(despesasList);
+    }
 
 }
