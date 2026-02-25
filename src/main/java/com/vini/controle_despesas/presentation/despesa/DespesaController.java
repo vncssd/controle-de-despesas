35263d9a6d1394d8727a5090c97a91c7c6462a8d
Vincius <viniviniee11@gmail.com>
2026-02-25 08:27:47 -0300

    feat: cria o endpoint para listar despesa por id

--- src/main/java/com/vini/controle_despesas/presentation/despesa/DespesaController.java
+++ src/main/java/com/vini/controle_despesas/presentation/despesa/DespesaController.java
@@ -3,7 +3,9 @@ package com.vini.controle_despesas.presentation.despesa;
 
 import com.vini.controle_despesas.application.despesa.DespesaDTO;
 import com.vini.controle_despesas.domain.despesa.usecase.adicionar.AdicionarDespesaUseCase;
+import com.vini.controle_despesas.domain.despesa.usecase.apagar.ApagarDespesaUseCase;
 import com.vini.controle_despesas.domain.despesa.usecase.listar.ListarDespesasUseCase;
+import com.vini.controle_despesas.domain.despesa.usecase.listarPorId.ListarDespesaPorIdUseCase;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +16,15 @@ import java.util.List;
 @RequestMapping("/despesa")
 public class DespesaController {
 
+    private final ListarDespesaPorIdUseCase listarDespesaPorIdUseCase;
     private AdicionarDespesaUseCase adicionarDespesaUseCase;
     private ListarDespesasUseCase listarDespesasUseCase;
+    private ApagarDespesaUseCase apagarDespesaUseCase;
 
-    public DespesaController(AdicionarDespesaUseCase adicionarDespesaUseCase, ListarDespesasUseCase listarDespesasUseCase) {
+    public DespesaController(AdicionarDespesaUseCase adicionarDespesaUseCase, ListarDespesasUseCase listarDespesasUseCase, ListarDespesaPorIdUseCase listarDespesaPorIdUseCase) {
         this.adicionarDespesaUseCase = adicionarDespesaUseCase;
         this.listarDespesasUseCase = listarDespesasUseCase;
+        this.listarDespesaPorIdUseCase = listarDespesaPorIdUseCase;
     }
 
     @PostMapping("/adicionar")
@@ -34,6 +39,20 @@ public class DespesaController {
         List<DespesaDTO> despesasList = listarDespesasUseCase.execute();
         return ResponseEntity.status(HttpStatus.OK)
                 .body(despesasList);
+    }
+
+    @DeleteMapping("/apagar/{id}")
+    public ResponseEntity<String> apagarPorId (@PathVariable Long id){
+        apagarDespesaUseCase.execute(id);
+
+        if (listarDespesaPorIdUseCase.execute(id) != null){
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("despesa apagada id: " + id);
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("despesa com id " +id+ " não encontrada");
+        }
     }
 
 }
