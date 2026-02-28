e68917e33f2bde6146f8032ece618329db30c0c2
Vincius <viniviniee11@gmail.com>
2026-02-27 22:32:55 -0300

    feat: cria o model de parcelamento e seus campos

--- src/main/java/com/vini/controle_despesas/domain/parcelamento/model/ParcelamentoModel.java
+++ src/main/java/com/vini/controle_despesas/domain/parcelamento/model/ParcelamentoModel.java
@@ -1,7 +1,12 @@
 package com.vini.controle_despesas.domain.parcelamento.model;
 
 import com.vini.controle_despesas.domain.despesa.model.DespesaModel;
+import com.vini.controle_despesas.domain.parcelamento.enums.ParcelamentoJurosTipo;
+import com.vini.controle_despesas.domain.parcelamento.enums.ParcelamentoStatus;
 import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Table(name="tb_parcelamento")
@@ -11,10 +16,25 @@ public class ParcelamentoModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(mappedBy = "parcelamento")
-    private DespesaModel despesa;
+    @Column (name = "status_parcelamento")
+    private ParcelamentoStatus parcelamentoStatus;
 
+    @Column (name = "valor_original")
+    private BigDecimal valorOriginal;
 
+    @Column (name = "valor_parcela")
+    private BigDecimal valorParcela;
 
+    @Column (name = "valor_parcelado")
+    private BigDecimal valorParcelado;
+
+    @Column (name = "parcelas_pagas")
+    private Integer parcelasPagas;
+
+    @Column (name = "data_inicio")
+    private LocalDate dataInicio;
+
+    @Column (name = "tipo_juros")
+    private ParcelamentoJurosTipo tipoJuros;
 
 }
