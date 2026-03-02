e80052097d44df834570a5abd2a358737033c886
Vincius <viniviniee11@gmail.com>
2026-03-02 10:07:52 -0300

    feat: injeta a dependencia de CalcularValorTotalService no usecase

--- src/main/java/com/vini/controle_despesas/domain/despesa/usecase/adicionar/AdicionarDespesaUseCaseImpl.java
+++ src/main/java/com/vini/controle_despesas/domain/despesa/usecase/adicionar/AdicionarDespesaUseCaseImpl.java
@@ -1,23 +1,25 @@
 package com.vini.controle_despesas.domain.despesa.usecase.adicionar;
 
+import com.vini.controle_despesas.domain.despesa.enums.DespesaTipo;
 import com.vini.controle_despesas.domain.despesa.model.DespesaModel;
 import com.vini.controle_despesas.application.despesa.DespesaDTO;
 import com.vini.controle_despesas.application.despesa.DespesaMapper;
+import com.vini.controle_despesas.domain.parcelamento.service.CalcularValorTotalService;
 import com.vini.controle_despesas.infrastructure.despesa.DespesaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 public class AdicionarDespesaUseCaseImpl implements AdicionarDespesaUseCase {
 
     private DespesaMapper despesaMapper;
     private DespesaRepository despesaRepository;
+    private CalcularValorTotalService calcularValorTotalService;
 
-    public AdicionarDespesaUseCaseImpl(DespesaMapper despesaMapper, DespesaRepository despesaRepository) {
+    public AdicionarDespesaUseCaseImpl(DespesaMapper despesaMapper, DespesaRepository despesaRepository, CalcularValorTotalService calcularValorTotalService) {
         this.despesaMapper = despesaMapper;
         this.despesaRepository = despesaRepository;
+        this.calcularValorTotalService = calcularValorTotalService;
     }
 
     @Override
