8134e56ff89c4b045324f970b015431de0c18b81
Vincius <viniviniee11@gmail.com>
2026-03-02 16:08:57 -0300

    fix: adiciona a annotation @RequestMappinp("/despesa") para padronização de endpoint

--- src/main/java/com/vini/controle_despesas/presentation/controller/despesa/AdicionarController.java
+++ src/main/java/com/vini/controle_despesas/presentation/controller/despesa/AdicionarController.java
@@ -6,8 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/despesa")
 @RestController
 public class AdicionarController {
 
