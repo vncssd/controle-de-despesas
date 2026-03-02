e907eb2ca4b923418df3dbd6bfa18c188b51584b
Vincius <viniviniee11@gmail.com>
2026-03-02 10:08:49 -0300

    feat: adiciona os campos valorOriginal e parcelamento no DTO

--- src/main/java/com/vini/controle_despesas/application/despesa/DespesaDTO.java
+++ src/main/java/com/vini/controle_despesas/application/despesa/DespesaDTO.java
@@ -3,6 +3,7 @@ package com.vini.controle_despesas.application.despesa;
 import com.vini.controle_despesas.domain.despesa.enums.DespesaPrioridade;
 import com.vini.controle_despesas.domain.despesa.enums.DespesaStatus;
 import com.vini.controle_despesas.domain.despesa.enums.DespesaTipo;
+import com.vini.controle_despesas.domain.parcelamento.model.ParcelamentoModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,10 +19,12 @@ public class DespesaDTO {
 
     private Long id;
     private String descricao;
+    private BigDecimal valorOriginal;
     private BigDecimal valorTotal;
     private DespesaTipo tipo;
     private DespesaPrioridade prioridade;
     private DespesaStatus status;
     private LocalDate dataVencimento;
+    private ParcelamentoModel parcelamento;
 
 }
