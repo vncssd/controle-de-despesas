890c5b11902a62ab2d7a2cd2b0b705e9af5f9eb1
Vincius <viniviniee11@gmail.com>
2026-03-02 10:11:16 -0300

    feat: cria o service para calcular valortotal em despesas parceladas

---
+++ src/main/java/com/vini/controle_despesas/domain/parcelamento/service/CalcularValorTotalService.java
@@ -0,0 +1,20 @@
+package com.vini.controle_despesas.domain.parcelamento.service;
+
+import com.vini.controle_despesas.domain.parcelamento.enums.ParcelamentoJurosTipo;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class CalcularValorTotalService {
+
+    private CalcularJurosService calcularJurosService;
+
+    public BigDecimal calcularValorTotal(ParcelamentoJurosTipo tipoJuros, BigDecimal valorOriginal, BigDecimal taxaDeJuros, Integer quantidadeParcelas){
+        return switch(tipoJuros){
+            case SIMPLES -> calcularJurosService.calcularJurosSimples(valorOriginal, taxaDeJuros, quantidadeParcelas);
+            case COMPOSTO -> calcularJurosService.calcularJurosCompostos(valorOriginal, taxaDeJuros, quantidadeParcelas);
+        };
+    }
+
+}
