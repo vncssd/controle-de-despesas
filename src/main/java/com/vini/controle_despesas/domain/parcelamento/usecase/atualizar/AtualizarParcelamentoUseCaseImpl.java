324be39acb290b059d2e7f5fab6b844c5d518827
Vincius <viniviniee11@gmail.com>
2026-03-03 15:44:33 -0300

    feat: implementa a interface do use case de atualizar um parcelamento

---
+++ src/main/java/com/vini/controle_despesas/domain/parcelamento/usecase/atualizar/AtualizarParcelamentoUseCaseImpl.java
@@ -0,0 +1,41 @@
+package com.vini.controle_despesas.domain.parcelamento.usecase.atualizar;
+
+import com.vini.controle_despesas.application.despesa.DespesaDTO;
+import com.vini.controle_despesas.application.despesa.DespesaMapper;
+import com.vini.controle_despesas.application.parcelamento.ParcelamentoDTO;
+import com.vini.controle_despesas.application.parcelamento.ParcelamentoMapper;
+import com.vini.controle_despesas.domain.despesa.model.DespesaModel;
+import com.vini.controle_despesas.domain.parcelamento.model.ParcelamentoModel;
+import com.vini.controle_despesas.infrastructure.despesa.DespesaRepository;
+import com.vini.controle_despesas.infrastructure.parcelamento.ParcelamentoRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class AtualizarParcelamentoUseCaseImpl implements AtualizarParcelamentoUseCase {
+
+    private ParcelamentoRepository parcelamentoRepository;
+    private ParcelamentoMapper parcelamentoMapper;
+
+    public AtualizarParcelamentoUseCaseImpl(ParcelamentoRepository parcelamentoRepository, ParcelamentoMapper parcelamentoMapper) {
+        this.parcelamentoRepository = parcelamentoRepository;
+        this.parcelamentoMapper = parcelamentoMapper;
+    }
+
+    @Override
+    @Transactional
+    public ParcelamentoDTO execute(Long id, ParcelamentoDTO parcelamentoDTO){
+        Optional<ParcelamentoModel> parcelamentoModelOptional = parcelamentoRepository.findById(id);
+        if (parcelamentoModelOptional.isEmpty()){
+            return null;
+        }
+        ParcelamentoModel parcelamentoAtualizado = parcelamentoMapper.map(parcelamentoDTO);
+        parcelamentoAtualizado.setId(id);
+        ParcelamentoModel parcelamentoSalvo = parcelamentoRepository.save(parcelamentoAtualizado);
+        return parcelamentoMapper.map(parcelamentoSalvo);
+    }
+
+
+}
