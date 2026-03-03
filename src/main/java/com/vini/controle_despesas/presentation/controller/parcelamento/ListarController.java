2228b03c5115bc1ee61039cadabd46a24c8a6fdf
Vincius <viniviniee11@gmail.com>
2026-03-03 15:44:47 -0300

    feat: cria o controller para listar parcelamentos

---
+++ src/main/java/com/vini/controle_despesas/presentation/controller/parcelamento/ListarController.java
@@ -0,0 +1,7 @@
+package com.vini.controle_despesas.presentation.controller.parcelamento;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ListarController {
+}
