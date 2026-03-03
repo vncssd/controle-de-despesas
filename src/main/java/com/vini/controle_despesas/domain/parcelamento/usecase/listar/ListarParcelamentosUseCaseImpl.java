e0d61050eb65d19990f2ee42780b0f36a308fccc
Vincius <viniviniee11@gmail.com>
2026-03-03 15:45:38 -0300

    feat: implementa a interface do use case de listar todos os parcelamentos

---
+++ src/main/java/com/vini/controle_despesas/domain/parcelamento/usecase/listar/ListarParcelamentosUseCaseImpl.java
@@ -0,0 +1,32 @@
+package com.vini.controle_despesas.domain.parcelamento.usecase.listar;
+
+import com.vini.controle_despesas.application.parcelamento.ParcelamentoDTO;
+import com.vini.controle_despesas.application.parcelamento.ParcelamentoMapper;
+import com.vini.controle_despesas.domain.despesa.model.DespesaModel;
+import com.vini.controle_despesas.domain.parcelamento.model.ParcelamentoModel;
+import com.vini.controle_despesas.infrastructure.parcelamento.ParcelamentoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ListarParcelamentosUseCaseImpl implements ListarParcelamentosUseCase{
+
+    private ParcelamentoRepository parcelamentoRepository;
+    private ParcelamentoMapper parcelamentoMapper;
+
+    public ListarParcelamentosUseCaseImpl(ParcelamentoRepository parcelamentoRepository, ParcelamentoMapper parcelamentoMapper) {
+        this.parcelamentoRepository = parcelamentoRepository;
+        this.parcelamentoMapper = parcelamentoMapper;
+    }
+
+    @Override
+    public List<ParcelamentoDTO> execute(){
+        List<ParcelamentoModel> parcelamentos = parcelamentoRepository.findAll();
+        return parcelamentos.stream()
+                .map(parcelamentoMapper::map)
+                .collect(Collectors.toList());
+    }
+
+}
