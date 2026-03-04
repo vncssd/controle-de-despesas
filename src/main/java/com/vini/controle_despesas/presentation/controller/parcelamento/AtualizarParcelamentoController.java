e4e3ec7d0ffec1f9e7ba6d1c362b93e901a44998
Vincius <viniviniee11@gmail.com>
2026-03-04 15:39:50 -0300

    feat: implementa a lógica de atualizar parcelamento no controller de atualizar parcelamento

--- src/main/java/com/vini/controle_despesas/presentation/controller/parcelamento/AtualizarParcelamentoController.java
+++ src/main/java/com/vini/controle_despesas/presentation/controller/parcelamento/AtualizarParcelamentoController.java
@@ -1,7 +1,30 @@
 package com.vini.controle_despesas.presentation.controller.parcelamento;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.vini.controle_despesas.application.parcelamento.ParcelamentoDTO;
+import com.vini.controle_despesas.domain.parcelamento.usecase.atualizar.AtualizarParcelamentoUseCase;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/parcelamento")
 public class AtualizarParcelamentoController {
+
+    private AtualizarParcelamentoUseCase atualizarParcelamentoUseCase;
+
+    public AtualizarParcelamentoController(AtualizarParcelamentoUseCase atualizarParcelamentoUseCase) {
+        this.atualizarParcelamentoUseCase = atualizarParcelamentoUseCase;
+    }
+
+    @PatchMapping("/atualizar/{id}")
+    public ResponseEntity<String> atualizar (@PathVariable Long id, @RequestBody ParcelamentoDTO parcelamentoDTO){
+        atualizarParcelamentoUseCase.execute(id, parcelamentoDTO);
+        if (atualizarParcelamentoUseCase != null){
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body("parcelamento com id "+id+ " atualizado!");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body("parcelamento com id "+id+ " atualizado!");
+    }
+
 }
