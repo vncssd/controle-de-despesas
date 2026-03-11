c6e09c734e121868b57a48eb37ee569f0f621dcc
Vincius <viniviniee11@gmail.com>
2026-03-11 13:30:12 -0300

    feat: adiciona o @Operation e o @ApiResponse para configuração do swagger

--- src/main/java/com/vini/controle_despesas/presentation/controller/despesa/AdicionarDespesaController.java
+++ src/main/java/com/vini/controle_despesas/presentation/controller/despesa/AdicionarDespesaController.java
@@ -2,6 +2,9 @@ package com.vini.controle_despesas.presentation.controller.despesa;
 
 import com.vini.controle_despesas.application.despesa.DespesaDTO;
 import com.vini.controle_despesas.domain.despesa.usecase.adicionar.AdicionarDespesaUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +21,22 @@ public class AdicionarDespesaController {
     public AdicionarDespesaController(AdicionarDespesaUseCase adicionarDespesaUseCase) {
         this.adicionarDespesaUseCase = adicionarDespesaUseCase;
     }
+
+    @Operation(
+            summary = "Adicionar despesa",
+            description = """
+            Cria uma nova despesa à vista ou parcelada.
+            
+            **Regras:**
+            - Despesa 'PARCELADA' deve conter o parcelamento preenchido
+            - Despesa 'A_VISTA' não pode conter parcelamento
+            """
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Despesa criada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Despesa à vista não pode ter parcelamento / Despesa parcelada deve conter os dados do parcelamento"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
 
     @PostMapping("/adicionar")
     public ResponseEntity<String> criarDespesa (@RequestBody DespesaDTO despesaDTO){
