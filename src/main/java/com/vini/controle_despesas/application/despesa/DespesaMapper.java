f86273ed003e72ab6e961d30772b6d39b78fd18f
Vincius <viniviniee11@gmail.com>
2026-06-07 13:38:48 -0300

    fix: corrige o parcelamento sendo gerado vazio no despesaMapper

--- src/main/java/com/vini/controle_despesas/application/despesa/DespesaMapper.java
+++ src/main/java/com/vini/controle_despesas/application/despesa/DespesaMapper.java
@@ -1,15 +1,18 @@
 package com.vini.controle_despesas.application.despesa;
 
+import com.vini.controle_despesas.application.parcelamento.ParcelamentoMapper;
 import com.vini.controle_despesas.domain.despesa.model.DespesaModel;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class DespesaMapper {
 
-    public DespesaModel map(DespesaDTO despesaDTO){
+    private final ParcelamentoMapper parcelamentoMapper;
 
+    public DespesaModel map(DespesaDTO despesaDTO) {
         DespesaModel despesaModel = new DespesaModel();
-
         despesaModel.setId(despesaDTO.getId());
         despesaModel.setDescricao(despesaDTO.getDescricao());
         despesaModel.setValorOriginal(despesaDTO.getValorOriginal());
@@ -18,15 +21,18 @@ public class DespesaMapper {
         despesaModel.setPrioridade(despesaDTO.getPrioridade());
         despesaModel.setStatus(despesaDTO.getStatus());
         despesaModel.setDataVencimento(despesaDTO.getDataVencimento());
-        despesaModel.setParcelamento(despesaModel.getParcelamento());
+
+        if (despesaDTO.getParcelamento() != null) {
+            despesaModel.setParcelamento(
+                    parcelamentoMapper.mapToModel(despesaDTO.getParcelamento())
+            );
+        }
 
         return despesaModel;
     }
 
-    public DespesaDTO map(DespesaModel despesaModel){
-
+    public DespesaDTO map(DespesaModel despesaModel) {
         DespesaDTO despesaDTO = new DespesaDTO();
-
         despesaDTO.setId(despesaModel.getId());
         despesaDTO.setDescricao(despesaModel.getDescricao());
         despesaDTO.setValorOriginal(despesaModel.getValorOriginal());
@@ -35,7 +41,12 @@ public class DespesaMapper {
         despesaDTO.setPrioridade(despesaModel.getPrioridade());
         despesaDTO.setStatus(despesaModel.getStatus());
         despesaDTO.setDataVencimento(despesaModel.getDataVencimento());
-        despesaDTO.setParcelamento(despesaDTO.getParcelamento());
+
+        if (despesaModel.getParcelamento() != null) {
+            despesaDTO.setParcelamento(
+                    parcelamentoMapper.mapToDTO(despesaModel.getParcelamento())
+            );
+        }
 
         return despesaDTO;
     }
